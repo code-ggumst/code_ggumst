da6240978cf59c3ea130e44c8e448b4b235071c9
JWCHOI <wkdudhksl@naver.com>
2022-03-26 17:13:21 +0900

    GET 테스트

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.codeggumst
 
-all = list(db.codeggumst.find({},{'_id':False}))
-for questions in all:
-    print(questions)
 
 # 메인페이지
 @app.route('/')
@@ -18,6 +15,11 @@ def main():
 @app.route('/start')
 def start():
     return render_template('start.html')
+
+@app.route('/quiz', methods=['GET'])
+def questions():
+    quiz = list(db.questions.find({}, {'_id': False}))
+    return jsonify({'all_questions':quiz})
 
 
 if __name__ == '__main__':
