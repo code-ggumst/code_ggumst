708bcbd8ff31634fb639ca35e3792a422e17c82a
Minjae <rjsekaehdhkw@gmail.com>
2022-03-27 20:16:15 +0900

    퀴즈 홈페이지 코드들

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.codeggumst
+
+all = list(db.codeggumst.find({},{'_id':False}))
+for questions in all:
+    print(questions)
+
+# 메인페이지
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+# 테스트 시작하기
+@app.route('/start')
+def start():
+    return render_template('start.html')
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
