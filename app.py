58ab303a7688e68c1fc2b1a36057d95d40c311db
kimyouu <oo1029oo@gmail.com>
2022-03-29 01:09:03 +0900

    프론트앤드, 백앤드 테스트

--- app.py
+++ app.py
@@ -19,6 +19,11 @@ def main():
 def start():
     return render_template('start.html')
 
+@app.route('/quiz', methods=['GET'])
+def questions():
+    quiz = list(db.questions.find({}, {'_id': False}))
+    return jsonify({'all_quiz': quiz})
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
