99e3dd641d0a0b8340eca0fe2ff35b3726b26a1f
JWCHOI <wkdudhksl@naver.com>
2022-03-30 18:01:15 +0900

    정오답 기능 테스트 (바디부분에서만 기능함)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.codeggumst
 
@@ -12,11 +14,19 @@ def main():
     return render_template('main.html')
 
 
-# 퀴즈 시작하기
+# 퀴즈 시작하기, 다시하기
 @app.route('/start')
 def start():
     return render_template('start.html')
 
+
+# 퀴즈 결과
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+
+# 퀴즈 리스트 보내기
 @app.route('/quiz', methods=['GET'])
 def questions():
     quiz = list(db.questions.find({}, {'_id': False}))
@@ -24,14 +34,17 @@ def questions():
     return jsonify({'all_quiz': quiz})
 
 
-# 정오답 체크하기
+# 정오답 체크하기                                현재 바디부분에 있는 버튼에서만 작동하고, 스크립트의 백틱내에 있는 버튼에는 작동안됨
 @app.route('/check', methods=['POST'])
 def answer_check():
-    answer_receive = request.form['answer_click']
-    if answer_receive == db.questions.find_one('correct') :
-        return jsonify({'msg':'정답입니다'})
-    else:
-        return jsonify({'msg':'오답입니다'})
+    answer_receive = request.form['answer_click']                       # 클릭한 버튼의 텍스트 가져오기
+    target_question = db.questions.find_one({'correct':answer_receive}) # 가져온 텍스트가 DB 정답열에 있다면 해당 정답을 가져옴
+    # print(answer_receive)
+    if target_question:                                                 # 정답을 가져왔다면 프론트에 정답 메세지
+        return jsonify({'msg': '정답입니다'})
+    else:                                                               # 가져오지 못했다면 프론트에 오답 메세지
+        return jsonify({'msg': '오답입니다'})
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
